485b716d59861f28dbf80674fcca8e942389964f
mytrofanov <mytrofanovmaxym@gmail.com>
2023-09-22 10:21:17 +0300

    feat: comments

--- __tests__/logos.test.js
+++ __tests__/logos.test.js
@@ -8,13 +8,10 @@ describe('Logos', () => {
     it('Logos renders images correctly', () => {
         render(<Logos />);
 
-        // Отримуємо всі елементи <img> в компоненті
         const images = screen.getAllByRole('img');
 
-        // Перевіряємо, чи знайдено хоча б один <img> елемент
         expect(images.length).toBeGreaterThan(0);
 
-        // Перевіряємо, чи кожен <img> має атрибути src та alt
         images.forEach(img => {
             expect(img).toHaveAttribute('src');
             expect(img).toHaveAttribute('alt');
